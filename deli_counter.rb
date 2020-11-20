40f3250d9e392215e0fee3b3809c0985381ea93b
TJZii <tjhaweszii@gmail.com>
2020-11-20 10:23:10 -0800

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,37 @@
 # Write your code here.
+def line(katz_deli)
+
+    lineorder = ["The line is currently:"]
+    order = 1
+
+    if katz_deli[0] == nil
+        puts "The line is currently empty."
+        return
+    end
+
+    katz_deli.each do |customer|
+        lineorder << "#{order}."
+        lineorder << customer
+        order += 1
+    end
+
+    puts lineorder.join(" ")
+
+end
+
+def take_a_number(katz_deli, name)
+    newline = katz_deli
+    position = katz_deli.size() + 1
+    newline << name
+    puts "Welcome, #{name}. You are number #{position} in line."
+    return newline
+end
+
+def now_serving(katz_deli)
+    if(katz_deli[0] == nil)
+        puts "There is nobody waiting to be served!"
+        return
+    end
+    puts "Currently serving #{katz_deli[0]}."
+    katz_deli.shift()
+end
